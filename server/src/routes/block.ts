40f58eba5bc2ababd090bc0216feb768948f0dc8
Alexandr-Horton <aleksandr.horton@gmail.com>
2026-04-13 12:11:50 +0300

    add blocks flow system with decomposition (components, reducers, facade), block creation, relations, rendering, and fixes

--- server/src/routes/block.ts
+++ server/src/routes/block.ts
@@ -1,20 +1,20 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const prisma = new PrismaClient();
 const router = Router();
 
 const Webhook = z.object({
   url: z.string().url(),
-  method: z.enum(["GET", "POST"]),
+  method: z.enum(['GET', 'POST']),
   query: z.string().optional(),
   headers: z.record(z.string()).optional(),
   body: z.string().optional(),
 });
 
-router.post("/", async (req, res) => {
-  if (req.body.type === "webhook") {
+router.post('/', async (req, res) => {
+  if (req.body.type === 'webhook') {
     const result = Webhook.safeParse(JSON.parse(req.body.data));
 
     if (!result.success) {
@@ -23,14 +23,55 @@ router.post("/", async (req, res) => {
     }
   }
 
-  const { name, type, data, processId } = req.body;
+  const { name, type, data, processId, x, y } = req.body;
   const newBlock = await prisma.processBlock.create({
-    data: { name, type, data, processId },
+    data: { name, type, data, processId, x, y },
   });
   res.json(newBlock);
 });
 
-router.get("/:id", async (req, res) => {
+const output = {
+  port: 'output',
+  label: 'Output',
+};
+
+const input = {
+  port: 'input',
+  label: 'Input',
+};
+
+const blockTypes = [
+  {
+    type: 'start',
+    label: 'Start',
+    outputs: [output],
+  },
+  {
+    type: 'webhook',
+    label: 'Call url',
+    inputs: [input],
+    outputs: [
+      {
+        port: 'success',
+        label: 'Success',
+      },
+      {
+        port: 'error',
+        label: 'Error',
+      },
+    ],
+  },
+  {
+    type: 'end',
+    inputs: [input],
+  },
+];
+
+router.get('/types', async (req, res) => {
+  res.json(blockTypes);
+});
+
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const block = await prisma.processBlock.findUnique({
     where: { id },
@@ -39,26 +80,26 @@ router.get("/:id", async (req, res) => {
   res.json(block);
 });
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  if (req.body.type === "webhook") {
+  if (req.body.type === 'webhook') {
     Webhook.parse(JSON.parse(req.body.data));
   }
-  const { name, type, data, processId } = req.body;
+  const { name, type, data, processId, x, y } = req.body;
   const updatedBlock = await prisma.processBlock.update({
     where: { id },
-    data: { name, type, data, processId },
+    data: { name, type, data, processId, x, y },
   });
   res.json(updatedBlock);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   await prisma.processBlock.delete({ where: { id } });
   res.sendStatus(204);
 });
 
-router.post("/relation", async (req, res) => {
+router.post('/relation', async (req, res) => {
   const { inputId, inputPort, outputId, outputPort } = req.body;
   const newRelation = await prisma.processBlockRelation.create({
     data: {
@@ -71,51 +112,10 @@ router.post("/relation", async (req, res) => {
   res.json(newRelation);
 });
 
-router.delete("/relation/:id", async (req, res) => {
+router.delete('/relation/:id', async (req, res) => {
   const { id } = req.params;
   await prisma.processBlockRelation.delete({ where: { id } });
   res.sendStatus(204);
-});
-
-const output = {
-  port: "output",
-  label: "Выход",
-};
-
-const input = {
-  port: "input",
-  label: "Вход",
-};
-
-router.get("/types", async (req, res) => {
-  return res
-    .json([
-      {
-        type: "start",
-        label: "Начало",
-        outputs: [output],
-      },
-      {
-        type: "webhook",
-        label: "Вызвать url",
-        inputs: [input],
-        outputs: [
-          {
-            port: "success",
-            label: "Успех",
-          },
-          {
-            port: "error",
-            label: "Ошибка",
-          },
-        ],
-      },
-      {
-        type: "end",
-        inputs: [input],
-      },
-    ])
-    .send();
 });
 
 export default router;
